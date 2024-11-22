5e2e9c3670db435fc2c1a401d716a3f4cee9af64
Pruthviraj-03 <pruthvirajkurane03@gmail.com>
2024-11-22 23:06:31 +0530

    cors related changes added

--- app.js
+++ app.js
@@ -9,23 +9,26 @@ dotenv.config({
 
 const app = express();
 
+// CORS Configuration
 app.use(
   cors({
-    origin: "https://finifitask.vercel.app/",
+    origin: ["https://finifitask.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+app.options("*", cors());
+
+// Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
+// Routes
 import { router as invoiceRouter } from "./src/routes/invoice.routes.js";
-
 app.use("/api", invoiceRouter);
-
-// http://localhost:8000/api/
 
 export { app };
